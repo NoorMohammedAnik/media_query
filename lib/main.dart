c33229f5fb7259613dd6862e7e58844d3c3c1171
NOOR MOHAMMED ANIK <noormohammedanik@gmail.com>
2020-11-20 09:40:50 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+  }
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height/5,
+            width: MediaQuery.of(context).size.width/4,
+            color: Colors.deepOrange,
+          ),
+        ),
+      ),
+    );
+  }
+}
